4214106c3db7c34b3ede222ab22675a1e77cdf7a
sdionisio4T <sdionisio4@gmail.com>
2026-02-22 15:12:42 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                              # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Daniel Dionisio",                       # Tu nombre
+    author_email="sdionisio4@gmail.com",            # Tu correo electrónico
+    url="https://github.com/sdionisio4T/tercerRepo",     # URL del proyecto
+)
